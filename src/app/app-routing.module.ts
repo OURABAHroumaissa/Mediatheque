54aa87dc2f7dbc065ffeac1bda706d681c313e6b
roumaissa OURABAH <er_ourabah@esi.dz>
2022-06-23 14:06:55 +0200

    feat:Create navBar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './commons/not-found/not-found.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { ProductsPageComponent } from './pages/products-page/products-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', redirectTo: 'Accueil',pathMatch: "full"
+  },
+  {
+    path:'Accueil',component: HomePageComponent
+  },
+  {
+    path:'Products',component: ProductsPageComponent
+  },
+  {
+    path:'**',component: NotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

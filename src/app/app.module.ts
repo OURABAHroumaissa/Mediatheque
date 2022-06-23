2e4f603d19ddcf629f89057ce4a3f4f1f3ce9ccc
roumaissa OURABAH <er_ourabah@esi.dz>
2022-06-23 16:37:37 +0200

    page ajout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,8 +12,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import {MatSidenavModule} from '@angular/material/sidenav';
-import { ProductListComponent } from './pages/product-list/product-list.component';
-import { ProductCardComponent } from './pages/product-card/product-card.component';
+import { ProductListComponent } from './pages/products-page/product-list/product-list.component';
+import { ProductCardComponent } from './pages/products-page/product-card/product-card.component';
 import {MatCardModule} from '@angular/material/card';
 
 @NgModule({

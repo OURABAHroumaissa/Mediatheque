70b98a0773941788137bb178c3251b25697f38e2
roumaissa OURABAH <er_ourabah@esi.dz>
2022-06-23 12:09:18 +0200

    git add .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './commons/nav-bar/nav-bar.component';
+import { FooterComponent } from './commons/footer/footer.component';
+import { NotFoundComponent } from './commons/not-found/not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    FooterComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,

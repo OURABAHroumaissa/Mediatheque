7fc9654d34157a4e25a73e7b170b381fdd08aaed
Ourabah Roumaissa <er_ourabah@esi.dz>
2022-06-26 14:09:11 +0200

    feat:Metttre en place la page HOME

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Album } from 'src/app/models/Album.model';
+import { Film } from 'src/app/models/Film.model';
+import { AlbumHttpService } from 'src/app/services/album/album-http.service';
+import { FilmHttpService } from 'src/app/services/film/film-http.service';
 
 @Component({
   selector: 'app-home-page',
@@ -6,8 +10,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent implements OnInit {
+  films: Film[] = [];
+  albums: Album[] = [];
 
-  constructor() { }
+  i:number=0;
+    
+  film1:string="Film";
+
+  slides = [
+    {
+      url: 'https://source.unsplash.com/1600x900/?nature,water'
+    },
+    {
+      url: 'https://source.unsplash.com/1600x1600/?nature,forest'
+    }
+  ]
+   
+  constructor(private serviceFilm:FilmHttpService,private serviceAlbum:AlbumHttpService) { 
+    this.serviceFilm.findAll().subscribe((data)=> this.films =data);
+    this.serviceAlbum.findAll().subscribe((data)=> this.albums=data);
+  }
 
   ngOnInit(): void {
   }
